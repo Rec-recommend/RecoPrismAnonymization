bab234c817fd954064355222d79748158b380fca
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-05-13 02:22:12 +0200

    Modify anonymize data

--- anonymize.py
+++ anonymize.py
@@ -1,15 +1,15 @@
 def anonymize( data , tableName ):
     tableHeader = tableName[0]
-    # print(tableHeader)
-    # print(data.columns[0])
-    data = renameColumnsHeader(data)
-        # print(column)
-    # print(data.columns)
+    data = renameColumnsHeader(data , tableHeader)
     return data
 
-def renameColumnsHeader(data):
+def renameColumnsHeader(data , tableHeader):
     for column in data.columns:
             data.rename(columns={column: column[0]}, inplace=True)
+            data = replaceColumnsData(data , column[0] ,tableHeader)
     return data
     
-def replaceColumnsData(data):
+def replaceColumnsData(data , columnName ,tableHeader):
+    for index , i in enumerate(data[columnName]):
+        data[columnName] = data[columnName].replace( i, tableHeader + columnName + str(index))
+    return data
--- dataPrepare.py
+++ dataPrepare.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from anonymize import anonymize
 from saveToCSV import saveDataToCSV
+
 df = pd.read_csv("./titanic_train.csv")
 
 data = anonymize( df , 'Users' )
