0defa7d4147aab6f0fcaddf9cc64ce83b8400c23
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-05-13 18:25:29 +0200

    Refactor code

--- dataPrepare.py
+++ dataPrepare.py
@@ -1,8 +1,33 @@
 import pandas as pd
+import mysql.connector
 
 from anonymize import anonymize
 from saveToCSV import saveDataToCSV
-def startAnonymize():
-    df = pd.read_csv("./preAnonymize.csv")
-    data = anonymize( df , 'Users' )
-    saveDataToCSV( data , 'Users' )
+
+def read_from_db(_host, _user, _password, _database , table_name):
+
+    db = mysql.connector.connect(
+        host = _host,
+        user = _user,
+        password = _password,
+        database = _database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM " + table_name)
+
+    df = pd.DataFrame(cursor.fetchall())
+
+    df.columns = cursor.column_names
+
+    db.close()
+
+    return df
+
+def read_from_csv(csv_path):
+    return pd.read_csv(csv_path)
+
+def startAnonymize(df, table_name):
+    data = anonymize( df , table_name )
+    saveDataToCSV( data , table_name )
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 
 from werkzeug import secure_filename
 from dataPrepare import *
+from config import *
+
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -17,7 +19,7 @@ def anonymize():
 @app.route('/anonymize',methods=['POST','GET'])
 def getWayToAnonymize():
     # if request.method == 'POST':
-        if str(request.form.get('way')) == "1":
+        if str(request.form.get('way')) == "csv":
             return render_template('anonymizeCSV.html')
         else:
             return render_template('anonymizeDB.html')
@@ -25,11 +27,26 @@ def getWayToAnonymize():
 @app.route('/anonymize/csv',methods=['POST','GET'])
 def anonymizeCSV():
     if request.method == 'POST':
-        f = request.files['file']
+        table_name  = request.form.get('table_name')
+        f           = request.files['file']
         f.save(secure_filename("preAnonymize.csv"))
-        startAnonymize()
-        return send_from_directory(app.root_path, filename="Users.csv")
-        # return render_template('anonymizeCSV.html', success=True)
+
+        df = read_from_csv(csv_path)
+        startAnonymize(df , table_name)
+        return send_from_directory(app.root_path, filename = table_name + ".csv")
+
+@app.route('/anonymize/db',methods=['POST','GET'])
+def anonymizeDB():
+    if request.method == 'POST':
+        host        = request.form.get('host')
+        user        = request.form.get('user')
+        password    = request.form.get('password')
+        database    = request.form.get('database')
+        table_name  = request.form.get('table_name')
+
+        df = read_from_db(host, user, password, database , table_name)
+        startAnonymize(df, table_name)
+        return send_from_directory(app.root_path, filename=table_name + ".csv")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True) 
+    app.run(host='127.0.0.1', debug=True)
