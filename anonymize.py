7fe45a234a6dddfcc62cb17a5b89b9f693a1b69e
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-05-13 19:57:14 +0200

    Fix wrong replacment of repeated chars

--- anonymize.py
+++ anonymize.py
@@ -1,15 +1,19 @@
+import pandas as pd
+
 def anonymize( data , tableName ):
     tableHeader = tableName[0]
     data = renameColumnsHeader(data , tableHeader)
     return data
 
 def renameColumnsHeader(data , tableHeader):
+    char= 'a'
     for column in data.columns:
-            data.rename(columns={column: column[0]}, inplace=True)
-            data = replaceColumnsData(data , column[0] ,tableHeader)
+        data.rename(columns={column: char}, inplace=True)
+        data = replaceColumnsData(data , char ,tableHeader)
+        char = chr(ord(char) + 1)
     return data
-    
-def replaceColumnsData(data , columnName ,tableHeader):
-    for index , i in enumerate(data[columnName]):
-        data[columnName] = data[columnName].replace( i, tableHeader + columnName + str(index))
+
+def replaceColumnsData(data , char ,tableHeader):
+    for index , i in enumerate(data[char]):
+        data[char] = data[char].replace( i, tableHeader + char + str(index))
     return data
