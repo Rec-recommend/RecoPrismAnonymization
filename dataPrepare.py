69dfdcca0d026b954c2e297fa112db23b0174922
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-05-13 16:31:18 +0200

    Modify dataPrepare

--- dataPrepare.py
+++ dataPrepare.py
@@ -2,9 +2,7 @@ import pandas as pd
 
 from anonymize import anonymize
 from saveToCSV import saveDataToCSV
-
-df = pd.read_csv("./titanic_train.csv")
-
-data = anonymize( df , 'Users' )
-
-saveDataToCSV( data , 'Users' )
+def startAnonymize():
+    df = pd.read_csv("./preAnonymize.csv")
+    data = anonymize( df , 'Users' )
+    saveDataToCSV( data , 'Users' )
