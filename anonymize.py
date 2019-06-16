7242815d5e0ccd2e63a870ef28a5cf048a78072b
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-16 20:59:00 +0200

    Update anonymizer algorithm

--- anonymize.py
+++ anonymize.py
@@ -1,26 +1,71 @@
 #!/usr/bin/python3
 import re
 import pandas as pd
+from abc import abstractmethod, ABC
+from sklearn.feature_extraction.text import CountVectorizer
 
-def anonymize( df , tableName ):
-    tableHeader = tableName[0]
-    df = renameColumnsHeader(df , tableHeader)
-    return df
+class Anonymizer(ABC):
+    def __init__(self, df):
+        self.df = df
 
-def renameColumnsHeader(df , tableHeader):
-    char= 'a'
-    for column in df.columns:
-        df.rename(columns={column: char}, inplace=True)
-        df = replaceColumnsData(df , char ,tableHeader)
-        char = chr(ord(char) + 1)
-    return df
+    def anonymize(self, tableName):
+        tableHeader = tableName[0]
+        self.df = self.renameColumnsHeader(tableHeader)
+        return self.df
 
-def replaceColumnsData(df , column_name ,tableHeader):
-    pattern = "^" + tableHeader + column_name + "[1-9]+$"
-    for index , value in enumerate(df[column_name]):
-        if not re.search(pattern, str(value)):
-            df[column_name] = df[column_name].replace( value, tableHeader + column_name + str(index))
-    return df
+    def renameColumnsHeader(self, tableHeader):
+        char = 'a'
+        for column in self.df.columns:
+            self.df.rename(columns={column: char}, inplace=True)
+            self.replaceColumnsData(char, tableHeader)
+            char = chr(ord(char) + 1)
+        return self.df
 
-df = pd.read_csv("./titanic_train.csv")
-print(anonymize(df, "users").head(25))
+    @abstractmethod
+    def replaceColumnsData(self, column_name, tableHeader):
+        pass
+
+
+
+
+class UserAnonymizer(Anonymizer):
+    def __init__(self, df):
+        super().__init__(df)
+
+    def replaceColumnsData(self, column_name, tableHeader):
+        pattern = "^" + tableHeader + column_name + "[1-9]+$"
+        for index, value in enumerate(self.df[column_name]):
+            if not re.search(pattern, str(value)):
+                self.df[column_name].replace({value: tableHeader + column_name + str(index)}, inplace=True)
+        return self.df
+
+
+
+
+class ItemAnonymizer(Anonymizer):
+    def __init__(self, df):
+        super().__init__(df)
+
+    def replaceColumnsData(self, column_name ,tableHeader):
+        self.df[column_name] = self.df[column_name].astype(str)
+        words_anonymization = {}
+        vectorizer  = CountVectorizer(token_pattern=r'(?u)\b\w+\b')
+        analyze     = vectorizer.build_analyzer()
+        cleaned_words = []
+
+        for row_index, value in enumerate(self.df[column_name]):
+            cleaned_words = analyze(value)
+            for wordNum , word in enumerate(cleaned_words):
+                if word not in words_anonymization:
+                    words_anonymization[word] = tableHeader + column_name + str(row_index) + str(wordNum)
+
+                cleaned_words[wordNum] = words_anonymization[word]
+            self.df.at[row_index, column_name] = " ".join(str(x) for x in cleaned_words)
+
+        return self.df
+
+df = pd.read_csv("./movie_dataset_5000.csv")
+anon = ItemAnonymizer(df)
+data = anon.anonymize("users")
+print(data.head(50).to_string())
+data.to_csv('anon.csv')
