eaa9673bdf2f2e304b47b058fad2df7228f18946
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-16 21:47:28 +0200

    Fix invalid literal for int() with base 10

--- anonymize.py
+++ anonymize.py
@@ -16,56 +16,40 @@ class Anonymizer(ABC):
     def renameColumnsHeader(self, tableHeader):
         char = 'a'
         for column in self.df.columns:
-            self.df.rename(columns={column: char}, inplace=True)
-            self.replaceColumnsData(char, tableHeader)
-            char = chr(ord(char) + 1)
+            if not re.search("(?:.*id|index)", str(column), re.IGNORECASE):
+                self.replaceColumnsData(column)
+                char = chr(ord(char) + 1)
         return self.df
 
     @abstractmethod
     def replaceColumnsData(self, column_name, tableHeader):
         pass
 
-
-
-
-class UserAnonymizer(Anonymizer):
-    def __init__(self, df):
-        super().__init__(df)
-
-    def replaceColumnsData(self, column_name, tableHeader):
-        pattern = "^" + tableHeader + column_name + "[1-9]+$"
-        for index, value in enumerate(self.df[column_name]):
-            if not re.search(pattern, str(value)):
-                self.df[column_name].replace({value: tableHeader + column_name + str(index)}, inplace=True)
-        return self.df
-
-
-
-
 class ItemAnonymizer(Anonymizer):
     def __init__(self, df):
         super().__init__(df)
 
-    def replaceColumnsData(self, column_name ,tableHeader):
-        self.df[column_name] = self.df[column_name].astype(str)
+    def replaceColumnsData(self, column_name):
         words_anonymization = {}
         vectorizer  = CountVectorizer(token_pattern=r'(?u)\b\w+\b')
         analyze     = vectorizer.build_analyzer()
         cleaned_words = []
 
+        self.df[column_name] = self.df[column_name].astype(str)
         for row_index, value in enumerate(self.df[column_name]):
+            value = str(value)
             cleaned_words = analyze(value)
             for wordNum , word in enumerate(cleaned_words):
                 if word not in words_anonymization:
-                    words_anonymization[word] = tableHeader + column_name + str(row_index) + str(wordNum)
+                    words_anonymization[word] = column_name[0:2] + str(row_index) + str(wordNum)
 
                 cleaned_words[wordNum] = words_anonymization[word]
             self.df.at[row_index, column_name] = " ".join(str(x) for x in cleaned_words)
 
         return self.df
 
-df = pd.read_csv("./movie_dataset_5000.csv")
+df = pd.read_csv("./movies.csv")
 anon = ItemAnonymizer(df)
-data = anon.anonymize("users")
+data = anon.anonymize("items")
 print(data.head(50).to_string())
 data.to_csv('anon.csv')
